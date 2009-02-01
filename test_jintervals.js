95620b3b9f995ddbe61c62470667125f01c57b62
Rene Saarsoo <nene@triin.net>
2009-02-01 16:22:14 +0000

    Went over to LGPL license.
    GPL is too confusing in the context of JavaScript libraries.

--- test_jintervals.js
+++ test_jintervals.js
@@ -2,17 +2,18 @@
  * This file is part of jintervals.
  *
  * jintervals is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * jintervals is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with jintervals. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with jintervals. If not, see
+ * <http://www.gnu.org/licenses/>.
  * 
  * Copyright (c) 2009 Rene Saarsoo <http://code.google.com/p/jintervals/>
  *
