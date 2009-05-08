4ab08a2461a6c8459bfa3e8f637e594c9d96c59c
Rene Saarsoo <nene@triin.net>
2009-05-08 11:22:48 +0000

    Separated parsing and interpretation stage of interval formatting. Reorganized code to three classes.

--- jintervals.js
+++ jintervals.js
@@ -19,69 +19,6 @@
  *
  */
 var jintervals = (function() {
-  // utility function to pad number with leading zeros
-  function zeropad(nr, decimals) {
-    var padLength = decimals - (""+nr).length;
-    return (padLength > 0) ? repeat("0", padLength) + nr : nr;
-  }
-
-  // utility function to repeat string
-  function repeat(string, times) {
-    var result = "";
-    for (var i=0; i < times; i++) {
-      result += string;
-    }
-    return result;
-  }
-
-  // interprets a single interval code -- the text between { and }
-  // returns the corresponding value
-  function evalCode(code, t) {
-    var re = /^[{]([smhdg])([smhdg]*)?(ays?|ours?|inutes?|econds?|reatests?|\.)?(\?(.*))?[}]$/i;
-    var matches = re.exec(code);
-    if (!matches) {
-      return "?";
-    }
-    
-    var type = (matches[1].toUpperCase() == "G") ? t.G : matches[1];
-    var value = t.hasOwnProperty(type) ? t[type] : "?";
-    var paddingLength = (matches[2] || "").length + 1;
-    var suffix = getSuffix((matches[3] || ""), type, value);
-    var optional = !!matches[4];
-    var optionalSuffix = matches[5] || "";
-    
-    // hide when ? modifier is present and totalvalue is zero
-    if (optional && t[type.toUpperCase()] == 0) {
-      return "";
-    }
-    
-    return zeropad(value, paddingLength) + suffix + optionalSuffix;
-  }
-  
-  function getSuffix(suffixType, type, value) {
-    var lcType = type.toLowerCase();
-    
-    if (suffixType == "") {
-      return "";
-    }
-    else if (suffixType == ".") {
-      return getLetterSuffix(lcType);
-    }
-    else {
-      return getFullSuffix(lcType, value);
-    }
-  }
-  
-  function getLetterSuffix(lcType) {
-    return locales[currentLocale].letter[lcType];
-  }
-                    
-  function getFullSuffix(lcType, value) {
-    var unit = locales[currentLocale];
-    var pluralForm = unit.plural(value);
-    return unit.full[lcType][pluralForm];
-  }
-
   function jintervals(seconds, format) {
     // precalculate all different intervals we might need
     var t = {};
@@ -97,81 +34,175 @@ var jintervals = (function() {
     t.G = t.d ? "d" : (t.h ? "h" : (t.m ? "m" : "s"));
     t.g = t.G;
     
-    
-    var unparsed = format;
-    var result = "";
-    while ( unparsed.length > 0 ) {
-      // leave plain text untouched
-      var textMatch = /^([^\\{]+)([\\{].*|)$/.exec(unparsed);
-      if (textMatch) {
-        result += textMatch[1];
-        unparsed = textMatch[2];
-      }
-      
-      // replace interval placeholder
-      var match = /^([{].*?(?:[}]|$))(.*)$/i.exec(unparsed);
-      if (match) {
-        result += evalCode(match[1], t);
-        unparsed = match[2];
-      }
-
-      // backslash escapes next character
-      // transform \{ --> {
-      // transform \\ --> \
-      if (unparsed.charAt(0) === "\\") {
-        result += unparsed.charAt(1);
-        unparsed = unparsed.slice(2);
-      }
-    }
-    
-    return result;
+    return Interpreter.evaluate(t, Parser.parse(format));
   };
       
-  var locales = {
-    en_US: {
-      letter: {
-        d: "d",
-        h: "h",
-        m: "m",
-        s: "s"
-      },
-      full: {
-        d: [" day", " days"],
-        h: [" hour", " hours"],
-        m: [" minute", " minutes"],
-        s: [" second", " seconds"]
-      },
-      plural: function(nr) {
-        return (nr == 1) ? 0 : 1;
+  /**
+   * Parses format string into data structure,
+   * that can be interpreted later.
+   */
+  var Parser = {
+    parse: function(format) {
+      var unparsed = format;
+      var result = [];
+      while ( unparsed.length > 0 ) {
+        // leave plain text untouched
+        var textMatch = /^([^\\{]+)([\\{].*|)$/.exec(unparsed);
+        if (textMatch) {
+          result.push(textMatch[1]);
+          unparsed = textMatch[2];
+        }
+        
+        // parse jintervals {Code} separately
+        var match = /^([{].*?(?:[}]|$))(.*)$/i.exec(unparsed);
+        if (match) {
+          result.push(this.parseCode(match[1]));
+          unparsed = match[2];
+        }
+        
+        // backslash escapes next character
+        // transform \{ --> {
+        // transform \\ --> \
+        if (unparsed.charAt(0) === "\\") {
+          result.push(unparsed.charAt(1));
+          unparsed = unparsed.slice(2);
+        }
       }
+      return result;
     },
-    et_EE: {
-      letter: {
-        d: "p",
-        h: "h",
-        m: "m",
-        s: "s"
-      },
-      full: {
-        d: [" p\u00E4ev", " p\u00E4eva"],
-        h: [" tund", " tundi"],
-        m: [" minut", " minutit"],
-        s: [" sekund", " sekundit"]
-      },
-      plural: function(nr) {
-        return (nr == 1) ? 0 : 1;
+    
+    // parses single {Code} in format string
+    // Returns object representing the code or false when incorrect format string
+    parseCode: function(code) {
+      var re = /^[{]([smhdg])([smhdg]*)?(ays?|ours?|inutes?|econds?|reatests?|\.)?(\?(.*))?[}]$/i;
+      var matches = re.exec(code);
+      if (!matches) {
+        return false;
       }
+      
+      return {
+        type: matches[1],
+        paddingLength: (matches[2] || "").length + 1,
+        format: (matches[3]||"") == "" ? false : (matches[3] == "." ? "letter" : "full"),
+        optional: !!matches[4],
+        optionalSuffix: matches[5] || ""
+      };
+    }
+    
+  };
+      
+  /**
+   * Evaluates parse tree in the context of given time interval
+   */
+  var Interpreter = {
+    evaluate: function(time, parseTree) {
+      var result = "";
+      while ( parseTree.length > 0 ) {
+        var code = parseTree.shift();
+        // leave plain text untouched
+        if (typeof code === "string") {
+          result += code;
+        }
+        
+        // evaluate the code
+        else if (typeof code === "object") {
+          var type = (code.type.toUpperCase() == "G") ? time.G : code.type;
+          var value = time.hasOwnProperty(type) ? time[type] : "?";
+          var suffix = code.format ? Localization.translate(code.format, type.toLowerCase(), value) : "";
+          
+          // show when not optional or totalvalue is non-zero
+          if (!code.optional || time[type.toUpperCase()] != 0) {
+            result += this.zeropad(value, code.paddingLength) + suffix + code.optionalSuffix;
+          }
+        }
+        
+        // otherwise we have error
+        else {
+          result += "?";
+        }
+      }
+      return result;
+    },
+    
+    // utility function to pad number with leading zeros
+    zeropad: function(nr, decimals) {
+      var padLength = decimals - (""+nr).length;
+      return (padLength > 0) ? this.repeat("0", padLength) + nr : nr;
+    },
+    
+    // utility function to repeat string
+    repeat: function(string, times) {
+      var result = "";
+      for (var i=0; i < times; i++) {
+        result += string;
+      }
+      return result;
     }
   };
   
-  var currentLocale = "en_US";
+  
+  var Localization = {
+    translate: function(format, lcType, value) {
+      var loc = this.locales[this.currentLocale];
+      var translation = loc[format][lcType];
+      if (typeof translation === "string") {
+        return translation;
+      }
+      else {
+        return translation[loc.plural(value)];
+      }
+    },
+    
+    locale: function(loc) {
+      if (loc) {
+        this.currentLocale = loc;
+      }
+      return this.currentLocale;
+    },
+    
+    currentLocale: "en_US",
+    
+    locales: {
+      en_US: {
+        letter: {
+          d: "d",
+          h: "h",
+          m: "m",
+          s: "s"
+        },
+        full: {
+          d: [" day", " days"],
+          h: [" hour", " hours"],
+          m: [" minute", " minutes"],
+          s: [" second", " seconds"]
+        },
+        plural: function(nr) {
+          return (nr == 1) ? 0 : 1;
+        }
+      },
+      et_EE: {
+        letter: {
+          d: "p",
+          h: "h",
+          m: "m",
+          s: "s"
+        },
+        full: {
+          d: [" p\u00E4ev", " p\u00E4eva"],
+          h: [" tund", " tundi"],
+          m: [" minut", " minutit"],
+          s: [" sekund", " sekundit"]
+        },
+        plural: function(nr) {
+          return (nr == 1) ? 0 : 1;
+        }
+      }
+    }
+  };
   
   // Changing and getting current locale
   jintervals.locale = function(loc) {
-    if (loc) {
-      currentLocale = loc;
-    }
-    return currentLocale;
+    return Localization.locale(loc);
   };
   
   return jintervals;
